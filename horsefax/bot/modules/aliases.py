49c2fcdae7cbfdd56deea908540ce0b0530032b1
Katharine Berry <katharine@kathar.in>
2017-08-05 01:04:52 -0700

    Added /removealias

--- horsefax/bot/modules/aliases.py
+++ horsefax/bot/modules/aliases.py
@@ -1,4 +1,5 @@
 import copy
+from typing import Optional
 from peewee import *
 
 from horsefax.telegram.services.command import Command
@@ -19,12 +20,13 @@ class PingModule(BaseModule):
         self.bot = bot
         self.util = util
         self.util.register_command('addalias', self.add_alias)
+        self.util.register_command('removealias', self.remove_alias)
         for alias in Alias.select():
             self.util.register_command(alias.alias, self.handle_alias)
 
-    def add_alias(self, command: Command):
+    def add_alias(self, command: Command) -> Optional[str]:
         if len(command.args) < 2:
-            return "`Syntax: /addalias <alias> <command>`"
+            return "Syntax: `/addalias <alias> <command>`"
         alias = command.args[0]
         alias_to = ' '.join(command.args[1:])
         try:
@@ -32,9 +34,9 @@ class PingModule(BaseModule):
         except IntegrityError:
             return "That alias already exists."
         self.util.register_command(alias, self.handle_alias)
-        return f"Added alias `{alias}`."
+        return f"Added alias /{alias}."
 
-    def handle_alias(self, command: Command):
+    def handle_alias(self, command: Command) -> Optional[str]:
         alias = Alias.get(alias=command.command)
         if not hasattr(command.message, 'alias_origin'):
             command.message.alias_origin = []
@@ -47,3 +49,14 @@ class PingModule(BaseModule):
         if len(command.args) > 0:
             new_message.text += ' ' + ' '.join(command.args)
         self.bot.commands.handle_message(new_message)
+
+    def remove_alias(self, command: Command) -> str:
+        if len(command.args) < 1:
+            return "Syntax: `/removealias alias`"
+        alias_name = command.args[0]
+        try:
+            alias = Alias.get(alias=alias_name)
+        except DoesNotExist:
+            return f"Alias `{alias_name}` does not exist."
+        alias.delete()
+        return f"Deleted `/{alias_name}` (which was an alias for `/{alias.command}`)."
