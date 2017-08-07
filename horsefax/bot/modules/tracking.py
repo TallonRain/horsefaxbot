3621971e043a5483d209231f43d975adcad8a869
Katharine Berry <katharine@kathar.in>
2017-08-06 18:38:16 -0700

    Log things.

--- horsefax/bot/modules/tracking.py
+++ horsefax/bot/modules/tracking.py
@@ -1,12 +1,14 @@
+import datetime
 from pony.orm import *
 from pony import orm
-from typing import Optional
+from typing import Optional, Dict, Union
 
 
 from ..core import HorseFaxBot, ModuleTools, BaseModule
 from ..db import db
 from horsefax.telegram.types import (Message, User, Chat, UsersJoinedMessage, UserLeftMessage, ChatMigrateFromIDMessage,
-                                     MessagePinnedMessage, TextMessage)
+                                     MessagePinnedMessage, TextMessage, TextEntity, PhotoMessage, StickerMessage,
+                                     VideoMessage, VideoNoteMessage, DocumentMessage, AudioMessage, PhotoSize)
 
 
 class TelegramUser(db.Entity):
@@ -16,6 +18,8 @@ class TelegramUser(db.Entity):
     last_name = orm.Optional(str, nullable=True)
     language_code = orm.Optional(str, nullable=True)
     chats = Set('TelegramChat')
+    sent_messages = Set('TelegramMessage', reverse="sender")
+    forwarded_messages = Set('TelegramMessage', reverse="forward_from")
 
     def to_user(self):
         return User({'id': self.id,
@@ -32,6 +36,63 @@ class TelegramChat(db.Entity):
     all_members_are_administrators = Required(bool)
     users = Set(TelegramUser)
     pinned_message = orm.Optional(int)
+    messages = Set('TelegramMessage', reverse="chat")
+
+
+class TelegramMessage(db.Entity):
+    id = PrimaryKey(int)
+    sender = Required(TelegramUser, reverse="sent_messages")
+    date = Required(datetime.datetime, index=True)
+    chat = orm.Optional(TelegramChat)
+    forward_from = orm.Optional(TelegramUser, reverse="forwarded_messages")
+    reply_to = orm.Optional('TelegramMessage')
+    replies = Set('TelegramMessage')
+    edit_date = orm.Optional(datetime.datetime)
+
+
+class TelegramTextMessage(TelegramMessage):
+    text = Required(str)
+    entities = Required(Json)
+
+
+class FileMessage(TelegramMessage):
+    file_id = Required(str)
+    mime_type = orm.Optional(str, nullable=True)
+    file_size = orm.Optional(int)
+    caption = orm.Optional(str, nullable=True)
+    thumbnail = orm.Optional(str, nullable=True)
+
+
+class VisualMessage(FileMessage):
+    width = Required(int)
+    height = Required(int)
+
+
+class LongMessage(FileMessage):
+    duration = Required(int)
+
+
+class TelegramPhotoMessage(VisualMessage):
+    pass
+
+
+class TelegramStickerMessage(VisualMessage):
+    emoji = orm.Optional(str, nullable=True)
+
+
+class TelegramVideoMessage(LongMessage, VisualMessage):
+    pass
+
+
+class TelegramVideoNoteMessage(LongMessage, VisualMessage):
+    pass
+
+class TelegramDocumentMessage(FileMessage):
+    file_name = orm.Optional(str, nullable=True)
+
+class TelegramAudioMessage(LongMessage):
+    performer = orm.Optional(str, nullable=True)
+    title = orm.Optional(str, nullable=True)
 
 
 class TrackingModule(BaseModule):
@@ -74,7 +135,52 @@ class TrackingModule(BaseModule):
 
         if isinstance(message, ChatMigrateFromIDMessage):
             TelegramChat[message.chat.id].users.add(TelegramChat[message.id].users)
-            TelegramChat[message.id].delete()
+            TelegramChat[message.id].users.clear()
+
+        if TelegramMessage.get(id=message.message_id):
+            return
+        # Handle logging
+        log_params = {'id': message.message_id,
+                      'sender': TelegramUser[message.sender.id],
+                      'date': message.date,
+                      'chat': TelegramChat[message.chat.id] if message.chat.type != Chat.Type.PRIVATE else None,
+                      'forward_from': TelegramUser[message.forward_from.id] if message.forward_from else None,
+                      'reply_to': TelegramMessage.get(id=message.reply_to_message.message_id) if message.reply_to_message is not None else None,
+                      'edit_date': message.edit_date}
+        if isinstance(message, TextMessage):
+            TelegramTextMessage(text=message.text,
+                                entities=[self._json_from_entity(x) for x in message.entities],
+                                **log_params)
+        elif isinstance(message, PhotoMessage):
+            big_photo = max(message.photo, key=lambda x: x.width * x.height)  # type: PhotoSize
+            if len(message.photo) > 1:
+                small_photo = min(message.photo, key=lambda x: x.width * x.height)  # type: PhotoSize
+                thumb = small_photo.file_id
+            else:
+                thumb = None
+            TelegramPhotoMessage(file_id=big_photo.file_id, file_size=big_photo.file_size, width=big_photo.width,
+                                 height=big_photo.height, caption=message.caption, mime_type="image/jpeg",
+                                 thumbnail=thumb, **log_params)
+        elif isinstance(message, StickerMessage):
+            TelegramStickerMessage(file_id=message.file_id, file_size=message.file_size, mime_type="image/webp",
+                                   width=message.width, height=message.height, emoji=message.emoji, **log_params)
+        elif isinstance(message, VideoMessage):
+            TelegramVideoMessage(file_id=message.file_id, file_size=message.file_size, mime_type=message.mime_type,
+                                 width=message.width, height=message.height, duration=message.duration,
+                                 thumbnail=message.thumbnail.file_id if message.thumbnail else None,
+                                 caption=message.caption, **log_params)
+        elif isinstance(message, VideoNoteMessage):
+            TelegramVideoNoteMessage(file_id=message.file_id, file_size=message.file_size, mime_type="video/mp4",
+                                     width=message.length, height=message.length,
+                                     thumbnail=message.thumbnail.file_id if message.thumbnail else None,
+                                     duration=message.duration, **log_params)
+        elif isinstance(message, DocumentMessage):
+            TelegramDocumentMessage(file_id=message.file_id, file_size=message.file_size, mime_type=message.mime_type,
+                                    thumbnail=message.thumbnail.file_id if message.thumbnail else None,
+                                    caption=message.caption, file_name=message.file_name, **log_params)
+        elif isinstance(message, AudioMessage):
+            TelegramAudioMessage(file_id=message.file_id, file_size=message.file_size, mime_type=message.mime_type,
+                                 performer=message.performer, title=message.title, **log_params)
 
     def update_chat(self, chat: Chat):
         try:
@@ -100,3 +206,15 @@ class TrackingModule(BaseModule):
         if user is None:
             return None
         return user.to_user()
+
+    def _json_from_entity(self, entity: TextEntity) -> Dict[str, Union[str, int]]:
+        ret = {
+            'type': entity.type.value,
+            'offset': entity.offset,
+            'length': entity.length,
+        }
+        if entity.url:
+            ret['url'] = entity.url
+        if entity.user:
+            ret['user'] = entity.user.id
+        return ret
