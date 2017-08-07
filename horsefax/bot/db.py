addeb2578e856b3b246b143087e660ab71cb0725
Katharine Berry <katharine@kathar.in>
2017-08-06 17:02:01 -0700

    Add tracking of chats.

--- horsefax/bot/db.py
+++ horsefax/bot/db.py
@@ -1,9 +1,27 @@
+from enum import Enum
 import horsefax.bot.config as config
 from pony.orm import *
+from pony.orm.dbapiprovider import StrConverter
+
+
+class EnumConverter(StrConverter):
+    def validate(self, val):
+        if not isinstance(val, Enum):
+            raise ValueError(f"Must be an Enum.  Got {type(val)}")
+        return val
+
+    def py2sql(self, val):
+        return val.name
+
+    def sql2py(self, value):
+        # Any enum type can be used, so py_type ensures the correct one is used to create the enum instance
+        return self.py_type[value]
+
 
 db = Database()
 
 
 def prepare_db():
     db.bind(config.db_provider, **config.db_params)
+    db.provider.converter_classes.append((Enum, EnumConverter))
     db.generate_mapping(create_tables=True)
--- horsefax/bot/modules/tracking.py
+++ horsefax/bot/modules/tracking.py
@@ -5,7 +5,8 @@ from typing import Optional
 
 from ..core import HorseFaxBot, ModuleTools, BaseModule
 from ..db import db
-from horsefax.telegram.types import Message, User
+from horsefax.telegram.types import (Message, User, Chat, UsersJoinedMessage, UserLeftMessage, ChatMigrateFromIDMessage,
+                                     MessagePinnedMessage)
 
 
 class TelegramUser(db.Entity):
@@ -14,6 +15,7 @@ class TelegramUser(db.Entity):
     first_name = Required(str)
     last_name = orm.Optional(str, nullable=True)
     language_code = orm.Optional(str, nullable=True)
+    chats = Set('TelegramChat')
 
     def to_user(self):
         return User({'id': self.id,
@@ -23,19 +25,59 @@ class TelegramUser(db.Entity):
                      'language_code': self.language_code})
 
 
-class UsersModule(BaseModule):
+class TelegramChat(db.Entity):
+    id = PrimaryKey(int, size=64)
+    type = Required(Chat.Type)
+    title = orm.Optional(str, nullable=True)
+    all_members_are_administrators = Required(bool)
+    users = Set(TelegramUser)
+    pinned_message = orm.Optional(int)
+
+
+class TrackingModule(BaseModule):
     def __init__(self, bot: HorseFaxBot, util: ModuleTools) -> None:
         self.bot = bot
         self.util = util
         self.bot.telegram.register_handler("message", self.handle_message)
 
+    @db_session
     def handle_message(self, message: Message) -> None:
+        # Track members
         origin = message.sender
         self.update_user(origin)
         if message.forward_from and isinstance(message.forward_from, User):
             self.update_user(message.forward_from)
         if message.reply_to_message:
             self.handle_message(message.reply_to_message)
+
+        # Track chats
+        if message.chat.type != Chat.Type.PRIVATE:
+            self.update_chat(message.chat)
+            TelegramChat[message.chat.id].users.add(TelegramUser[origin.id])
+
+        if isinstance(message, UsersJoinedMessage):
+            for user in message.users:
+                self.update_user(user)
+                TelegramChat[message.chat.id].users.add(TelegramUser[user.id])
+
+        if isinstance(message, UserLeftMessage):
+            self.update_user(message.user)
+            TelegramChat[message.chat.id].users.remove(TelegramUser[message.user.id])
+
+        if isinstance(message, MessagePinnedMessage):
+            TelegramChat[message.chat.id].pinned_message = message.message.message_id
+
+        if isinstance(message, ChatMigrateFromIDMessage):
+            TelegramChat[message.chat.id].users.add(TelegramChat[message.id].users)
+            TelegramChat[message.id].delete()
+
+    def update_chat(self, chat: Chat):
+        try:
+            TelegramChat[chat.id].set(title=chat.title, type=chat.type,
+                                      all_members_are_administrators=chat.all_members_are_administrators)
+        except ObjectNotFound:
+            TelegramChat(id=chat.id, type=chat.type, title=chat.title,
+                         all_members_are_administrators=chat.all_members_are_administrators)
 
     @db_session
     def update_user(self, user: User):
